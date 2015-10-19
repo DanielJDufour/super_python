d02691e268c81542daf2269e198dbf347e5c968f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2015-10-19 21:50:40 +0000

    added super_str and super_unicode

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'super_python',
+  packages = ['super_python'],
+  version = '0.2',
+  description = 'Make Your Python Super',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/super_python',
+  download_url = 'https://github.com/DanielJDufour/super_python/tarball/download',
+  keywords = ['python','str','unicode'],
+  classifiers = [],
+)
---
+++ super_python/__init__.py
@@ -0,0 +1,45 @@
+from re import compile as re_compile
+from re import sub as re_sub
+from re import UNICODE
+
+class super_str(str):
+    def remove(self, i):
+        if isinstance(i, str):
+            self.replace(i,'')
+        elif isinstance(i, list):
+            for e in i:
+                self = self.replace(e,'')
+        return self 
+
+    def lremove(self, i):
+        if isinstance(i, str):
+            return re_sub(re_compile('^' + i), '', self)
+        elif isinstance(i, list):
+            return re_sub(re_compile('^' + '(' + '|'.join(i) + ')'), '', self)
+
+    def rremove(self, i):
+        if isinstance(i, str):
+            return re_sub(re_compile(i + '$'), '', self)
+        elif isinstance(i, list):
+            return re_sub(re_compile('(' + '|'.join(i) + ')' + '$'), '', self)
+
+class super_unicode(unicode):
+    def remove(self, i):
+        if isinstance(i, unicode):
+            self.replace(i,u'')
+        elif isinstance(i, list):
+            for e in i:
+                self = self.replace(e,'')
+        return self 
+
+    def lremove(self, i):
+        if isinstance(i, unicode):
+            return re_sub(re_compile('^' + i, UNICODE), u'', self)
+        elif isinstance(i, list):
+            return re_sub(re_compile('^' + '(' + '|'.join(i) + ')', UNICODE), '', self)
+
+    def rremove(self, i):
+        if isinstance(i, unicode):
+            return re_sub(re_compile(i + '$', UNICODE), '', self)
+        elif isinstance(i, list):
+            return re_sub(re_compile('(' + '|'.join(i) + ')' + '$', UNICODE), '', self)
